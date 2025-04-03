07b146d67f7efbc82a94df863084406f18f8122b
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-03 17:27:52 -0300

    Add Components

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:4321",
   integrations: [tailwind(), partytown(), sitemap(), react()],
 });
