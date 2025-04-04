9d70ebb3109a9159ff03e6baa3ceb91b56f097bd
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-04 18:40:17 -0300

    Update Components

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -6,57 +6,42 @@ const homePage = {
 
     header: {
       stars: true,
-      imagePath: "/src/assets/peter-thomas-QiqdAtV9U18-unsplash.jpg",
-      imageAlt: "Peter Thomas Unsplash",
+      imagePath: "/src/assets/Direct_Fly_Alto_NG.jpg",
+      imageAlt: "Alto NG Air Plane",
       headerH1: `Learn to Fly with USATS`,
-      paragraph: "Paragraph",
+      paragraph: "Your path to becoming a professional pilot starts here. Our experienced instructors will help you achieve your aviation goals.",
       buttons: [
         {
           name: "First Button",
-          link: "",
-          primary: false,
+          link: "#",
+          primary: true,
         },
         {
           name: "Second Button",
-          link: "",
+          link: "#",
           primary: false,
         },
       ],
     },
 
     whyUs: {
-      upperHeading: "",
-      heading: "",
+      upperHeading: "Why WSAP",
+      heading: "Experience flight like never before. We're here to help you become the ultimate pilot.",
       descriptions: ["", ""],
       bullets: [
         {
-          title: "",
-          descriptions: [""],
-          icon: "",
+          title: "Quality of Instruction",
+          descriptions: ["Lorem ipsum dolor sit amet. Est assumenda alias et dignissimos modi sit suscipit consequuntur qui dolor quidem qui autem molestiae. Rem mollitia nostrum At reprehenderit labore et quia odit. Qui dolorem deleniti 33 fugiat nihil qui quas consequatur et cumque fugiat non nihil quia in nostrum laborum. Est distinctio labore sit maxime fuga 33 consequatur dignissimos qui quam modi id quidem voluptas."],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
         },
         {
-          title: "",
-          descriptions: [""],
-          icon: "",
-        },
-        {
-          title: "",
-          descriptions: [""],
-          icon: "",
-        },
-        {
-          title: "",
-          descriptions: [""],
-          icon: "",
-        },
-        {
-          title: "",
-          descriptions: [""],
-          icon: "",
+          title: "Personal Relationship",
+          descriptions: ["Lorem ipsum dolor sit amet. Est assumenda alias et dignissimos modi sit suscipit consequuntur qui dolor quidem qui autem molestiae. Rem mollitia nostrum At reprehenderit labore et quia odit. Qui dolorem deleniti 33 fugiat nihil qui quas consequatur et cumque fugiat non nihil quia in nostrum laborum. Est distinctio labore sit maxime fuga 33 consequatur dignissimos qui quam modi id quidem voluptas."],
+          icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
         },
       ],
       imageLegend: "",
-      imagePath: "",
+      imagePath: "/src/assets/USATS_banner_Application.webp",
       imageAlt: "",
     },
 
@@ -123,6 +108,21 @@ const homePage = {
         },
       ],
     },
+  },
+
+  frequentQuestions: {
+    title: "",
+    description: "",
+    questions: [
+      {
+        question: "Question 1",
+        answer: "ans 1",
+      },
+      {
+        question: "Question 2",
+        answer: "ans 2",
+      },
+    ],
   },
 };
 
