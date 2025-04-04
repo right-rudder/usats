72b0bf574cade6b58a56aba1197a8c2fabaff044
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-04 12:28:17 -0300

    Upload old website assets

--- src/consts.ts
+++ src/consts.ts
@@ -41,3 +41,12 @@ export const EMAIL_ADDRESS = "";
 // Pages Data
 
 export const BANNER_TITLE = "FlySchool Banner Title";
+
+// Video links
+
+export const VIDEOLINK_HOME = "https://cdn.shopify.com/videos/c/o/v/f7fe5a4231a94c38a91db2db8280be4e.mp4";
+export const VIDEOLINK_AIRTOUR = "https://cdn.shopify.com/videos/c/o/v/2e1eb45cc3e041bbb8f12d2b94d11067.mp4";
+export const VIDEOLINK_AIRPLANES = "https://cdn.shopify.com/videos/c/o/v/6b057798c5be41b9865d7a4054736b45.mp4";
+export const VIDEO_HOME_HELICOPTER = "https://cdn.shopify.com/videos/c/o/v/41e966eb47fa4d8eaad7f2b713b2459d.mp4";
+export const VIDEOLINK_MAINTENANCE = "https://cdn.shopify.com/videos/c/o/v/40ea4b0895e0415fa098733bcf6e8ee4.mp4";
+export const VIDEOLINK_ABOUTUS = "https://usatsflighttraining.com/cdn/shop/videos/c/vp/9cce912410e44240829a51b85a4ce623/9cce912410e44240829a51b85a4ce623.HD-1080p-2.5Mbps-42396578.mp4?v=0";
