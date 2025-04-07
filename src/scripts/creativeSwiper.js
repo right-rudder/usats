114e41d6a3f6304e1fc9f4522156e008b72ebf94
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-07 16:13:15 -0300

    Add Swiper

---
+++ src/scripts/creativeSwiper.js
@@ -0,0 +1,48 @@
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".creativeSwiper", {
+    watchSlidesProgress: true,
+    slidesPerView: 4,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    modules: [Navigation, Pagination, Autoplay],
+    autoplay: {
+      delay: 1800,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+    },
+    effect: "slide",
+  });
+});
