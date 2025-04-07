da21f270e40a23a798cdd7fce83cd781aa546290
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-07 14:24:35 -0300

    Fix overflow

--- src/data/ctaFlight.js
+++ src/data/ctaFlight.js
@@ -3,7 +3,7 @@ const CTAFlight = {
     imageAlt: "USATS student flying around the campus",
     headerH1: `Take the First Step</br>With a <span class="text-muted-900">Discovery Flight</span>`,
     paragraph:
-      "Launch your aviation journey with a Discovery Flight at US Aviation Training School! Take the controls and soar above Florida's stunning Space Coast—where rocket launches, sparkling beaches, and the shimmering Indian River Lagoon create a breathtaking backdrop for your first flight. Whether you're chasing the thrill of aviation as a passion or a career, our certified instructors will guide you through an unforgettable hands-on experience. Book your Discovery Flight today and turn your skyward dreams into reality!",
+      "Launch your aviation dreams at US Aviation Training School! Soar above  <strong>Florida's Space Coast - home to rocket launches, pristine beaches, and endless skies-</strong>  with a hands-on Discovery Flight guided by certified instructors. Book now and take control of your journey to the clouds",
     buttons: [
       {
         name: "Book Now",
