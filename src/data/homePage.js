e74589fc96fd4df91aaee658967e373796b8da40
Raul Ospina <raminka13@hotmail.com>
2025-04-07 17:47:17 -0500

    Update: reintegration

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -8,8 +8,9 @@ const homePage = {
       stars: true,
       imagePath: "/src/assets/Direct_Fly_Alto_NG.jpg",
       imageAlt: "Alto NG Air Plane",
-      headerH1: `Learn to Fly with USATS`,
-      paragraph: "Your path to becoming a professional pilot starts here. Our experienced instructors will help you achieve your aviation goals.",
+      headerH1: ` YOUR FLIGHT JOURNEY STARTS HERE`,
+      paragraph:
+        "Your path to becoming a professional pilot starts here. Our experienced instructors will help you achieve your aviation goals. We're here to help you become the ultimate pilot.",
       buttons: [
         {
           name: "Learn More",
@@ -23,9 +24,10 @@ const homePage = {
         },
       ],
     },
+
     underHeader: {
       title: "YOUR FLIGHT JOURNEY STARTS HERE",
-      paragraphs:[
+      paragraphs: [
         "Experience flight like never before. We're here to help you become the ultimate pilot.",
         "For over three decades, our organization has been at the forefront of helicopter pilot training, boasting a legacy that spans the globe. There are only a few places around the world where you will not find a pilot we had the privilege to mentor. Our commitment to excellence has empowered countless individuals to soar to new heights in their aviation careers.",
         "We have put many pilots with diverse backgrounds into airline cockpits. Our glass-cockpit airplanes and airline training environment will give you the knowledge and skills necessary to make you a highly desirable airline pilot candidate and allow for a smooth transition into your first airline job. ",
@@ -39,22 +41,23 @@ const homePage = {
           text: "About Us",
           link: "/#",
         },
-      ]
+      ],
     },
+
     ourAdvantages: {
       images: {
         panel: {
           imagePath: "/src/assets/ALTO_NG_Panel.jpg",
           imageAlt: "AirPlane Panel",
-          },
+        },
         h60Repair: {
           imagePath: "/src/assets/USATS_145_Repair_Station_UH-60.webp",
           imageAlt: "Two mechanic reparing a UH-60 Helicopter",
-          },
+        },
         base: {
           imagePath: "/src/assets/USATS_base_Cover.webp",
           imageAlt: "USATS base building",
-          },
+        },
       },
       buttons: [
         {
@@ -69,21 +72,21 @@ const homePage = {
         },
       ],
     },
-    
+
     yourCarrer: {
       images: {
         pilotSeat: {
           imagePath: "/src/assets/ALO_NG_INTERIOR.webp",
           imageAlt: "AirPlane Chair's",
-          },
+        },
         planeFront: {
           imagePath: "/src/assets/Happy_Landings_ALO_NG.webp",
           imageAlt: "The front of an airplane",
-          },
+        },
         dismantled: {
           imagePath: "/src/assets/ALTO_NG_KIT.jpg",
           imageAlt: "Dismantled airplane",
-          },
+        },
       },
       buttons: [
         {
@@ -104,19 +107,19 @@ const homePage = {
         h60: {
           imagePath: "/src/assets/UH-60_Black_Hawk_1.webp",
           imageAlt: "UH-60 Black Hawk Flying",
-          },
+        },
         hdx1200: {
           imagePath: "/src/assets/ALTO_NG.webp",
           imageAlt: "Dynon hdx1200",
-          },
+        },
         cesna172: {
           imagePath: "/src/assets/USATS_Plane_N3544W.webp",
           imageAlt: "One CESNA 172 parked in a ramp",
-          },
+        },
         s300: {
           imagePath: "/src/assets/Schweizer_S300_Usats_Ramp.webp",
           imageAlt: "Two S300 helicopters parked in a ramp",
-          },
+        },
       },
       buttons: [
         {
@@ -132,22 +135,30 @@ const homePage = {
       ],
     },
     whyUs: {
-      upperHeading: "Why WSAP",
-      heading: "Experience flight like never before. We're here to help you become the ultimate pilot.",
-      descriptions: ["", ""],
+      upperHeading: "Why USATS",
+      heading: "Being a Pilot starts now",
+      descriptions: [
+        "From flying Search and Rescue missions in the coastal regions of the Gulf of Mexico and the North Sea, providing VIP or recreational transport over the glacial fields of Alaska and the skyline of New York City, to flying military missions over the jungles of Colombia or Peru or the deserts of Africa, we have graduates flying in every corner of the globe.",
+      ],
       bullets: [
         {
           title: "Quality of Instruction",
-          descriptions: ["Lorem ipsum dolor sit amet. Est assumenda alias et dignissimos modi sit suscipit consequuntur qui dolor quidem qui autem molestiae. Rem mollitia nostrum At reprehenderit labore et quia odit. Qui dolorem deleniti 33 fugiat nihil qui quas consequatur et cumque fugiat non nihil quia in nostrum laborum. Est distinctio labore sit maxime fuga 33 consequatur dignissimos qui quam modi id quidem voluptas."],
+          descriptions: [
+            "Our experienced instructors are dedicated to providing the highest quality of instruction. We have a proven track record of success, with many of our graduates going on to have successful careers in aviation.",
+            "We are committed to providing the best possible training experience for our students. Our instructors are highly qualified and experienced, and they are dedicated to helping you achieve your aviation goals.",
+          ],
           icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
         },
         {
-          title: "Personal Relationship",
-          descriptions: ["Lorem ipsum dolor sit amet. Est assumenda alias et dignissimos modi sit suscipit consequuntur qui dolor quidem qui autem molestiae. Rem mollitia nostrum At reprehenderit labore et quia odit. Qui dolorem deleniti 33 fugiat nihil qui quas consequatur et cumque fugiat non nihil quia in nostrum laborum. Est distinctio labore sit maxime fuga 33 consequatur dignissimos qui quam modi id quidem voluptas."],
+          title: "Personal Development",
+          descriptions: [
+            "We pride ourselves on our personal relationships with our students. We understand that each student is unique, and we work hard to provide a personalized training experience that meets your individual needs.",
+            "Our instructors are dedicated to getting to know you and your goals, and they will work with you to develop a training plan that is tailored to your needs. We believe that this personal relationship is key to your success as a pilot.",
+          ],
           icon: "M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z",
         },
       ],
-      imageLegend: "",
+      imageLegend: "Become a Pilot with USATS",
       imagePath: "/src/assets/USATS_banner_Application.webp",
       imageAlt: "",
     },
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,7 +18,7 @@ export default {
         ],
       },
       colors: {
-        accent: {
+        muted: {
           50: "#fafafa",
           100: "#f4f4f5",
           200: "#e4e4e7",
@@ -44,7 +44,7 @@ export default {
           900: "#18181b",
           950: "#09090b",
         },
-        muted: {
+        accent: {
           50: "#fff1f2",
           100: "#ffe4e6",
           200: "#fecdd3",
