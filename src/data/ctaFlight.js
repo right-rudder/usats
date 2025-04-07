1666635d6cd96b25f456c268e78a7e2ef76584cd
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-07 14:04:16 -0300

    Add CTA Discovery Flight Component

---
+++ src/data/ctaFlight.js
@@ -0,0 +1,16 @@
+const CTAFlight = {
+    imagePath: "/src/assets/CTADiscFlights.png",
+    imageAlt: "USATS student flying around the campus",
+    headerH1: `Take the First Step</br>With a <span class="text-muted-900">Discovery Flight</span>`,
+    paragraph:
+      "Launch your aviation journey with a Discovery Flight at US Aviation Training School! Take the controls and soar above Florida's stunning Space Coast—where rocket launches, sparkling beaches, and the shimmering Indian River Lagoon create a breathtaking backdrop for your first flight. Whether you're chasing the thrill of aviation as a passion or a career, our certified instructors will guide you through an unforgettable hands-on experience. Book your Discovery Flight today and turn your skyward dreams into reality!",
+    buttons: [
+      {
+        name: "Book Now",
+        link: "/#",
+        primary: true,
+      },
+    ],
+  };
+  
+  export default CTAFlight;
