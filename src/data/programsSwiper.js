8e4becfe7eb83dc1e01c145f864105823b5ddb24
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-07 16:16:32 -0300

    Add Programs Swiper component

---
+++ src/data/programsSwiper.js
@@ -0,0 +1,100 @@
+const programsSwiper = {
+    upperHeading: "Explore",
+    heading: "Our Programs",
+    description:
+      "Your aviation dreams take center stage here. Whether pursuing a professional career, building flight hours, or seeking leisurely adventure, we have the perfect path for you.",
+    cards: [
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+      {
+        title: "Professional Pilot Programs",
+        imagePath: "/src/assets/ALTO_NG_Panel.jpg",
+        imageAlt:
+          "USATS  airline plane landing view from below",
+        description:
+          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+        link: "/#",
+      },
+    ],
+  };
+  
+  export default programsSwiper;
