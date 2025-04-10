39e1b4362f60359744755de83976b8108867982f
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-10 17:01:43 -0300

    Add flight training airplane pages

---
+++ src/data/coursesIndex.js
@@ -0,0 +1,35 @@
+const coursesIndex = {
+    data: {
+      // SEO Header Stuff
+      pageTitle: "Training Courses | US Academy ",
+      pageDescription:
+        "",
+      pageKeywords:
+        "",
+  
+      // Top Header
+      header: {
+        stars: true,
+        imagePath: "/src/assets/placeholder.webp",
+        imageAlt:
+          "image alt",
+        headerH1: `Pilot <br>PROGRAMS</span>`,
+        paragraph: ``,
+        buttons: [
+          {
+            name: "Enroll Today",
+            link: "/enroll-now",
+            primary: true,
+          },
+          {
+            name: "Program Guide",
+            link: "/path",
+            primary: false,
+          },
+        ],
+      },
+    },
+  };
+  
+  export default coursesIndex;
+  
