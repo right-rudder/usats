63a4cca094adf97df8d00183467bf4bc94a4efbd
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-14 12:54:59 -0300

    Update cta and underheader text colors

--- src/data/ctaFlight.js
+++ src/data/ctaFlight.js
@@ -1,7 +1,7 @@
 const CTAFlight = {
     imagePath: "/src/assets/CTADiscFlights.png",
     imageAlt: "USATS student flying around the campus",
-    headerH1: `Take the First Step</br>With a <span class="text-muted-900">Discovery Flight</span>`,
+    headerH1: `Take the First Step</br>With a <span class="text-accent-700">Discovery Flight</span>`,
     paragraph:
       "Launch your aviation dreams at US Aviation Training School! Soar above  <strong>Florida's Space Coast - home to rocket launches, pristine beaches, and endless skies-</strong>  with a hands-on Discovery Flight guided by certified instructors. Book now and take control of your journey to the clouds",
     buttons: [
