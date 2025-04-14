fb74ddd5dfed523e6e09bde916cee40cbd3ffc31
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-14 14:10:40 -0300

    Update helicopter programs index image

---
+++ src/data/coursesHelicopterIndex.js
@@ -0,0 +1,35 @@
+const coursesIndex = {
+    data: {
+      // SEO Header Stuff
+      pageTitle: "Training Courses | US Academy ",
+      pageDescription:
+        "",
+      pageKeywords:
+        "",
+  
+      // Top Header
+      header: {
+        stars: true,
+        imagePath: "/src/assets/UH-60_Black_Hawk_1.webp",
+        imageAlt:
+          "image alt",
+        headerH1: `Helicopter Pilot <br>PROGRAMS</span>`,
+        paragraph: ``,
+        buttons: [
+          {
+            name: "Enroll Now",
+            link: "/enroll-now",
+            primary: true,
+          },
+          {
+            name: "Program Guide",
+            link: "/path",
+            primary: false,
+          },
+        ],
+      },
+    },
+  };
+  
+  export default coursesIndex;
+  
