741e9735775bd5cccf32119754e8e19eb4a62bc7
Raul Ospina <raminka13@hotmail.com>
2025-04-15 09:34:24 -0500

    Update: const name

--- src/data/programs/ctaMilitary.js
+++ src/data/programs/ctaMilitary.js
@@ -1,22 +1,18 @@
-const CTAMilitary = {
-    imagePath: "/src/assets/UH-60_Black_Hawk_1.webp",
-    imageAlt: "UH-60 Black Hawk conducting NVG training mission near Kennedy Space Center",
-    headerH1: `Command the Night<br><span class="text-accent-700">Become a Tactical Flight Leader</span>`,
-    paragraph: `
+const militaryCta = {
+  imagePath: "/src/assets/UH-60_Black_Hawk_1.webp",
+  imageAlt:
+    "UH-60 Black Hawk conducting NVG training mission near Kennedy Space Center",
+  headerH1: `Command the Night<br><span class="text-accent-700">Become a Tactical Flight Leader</span>`,
+  paragraph: `
         Prove combat readiness at America’s premier tactical aviation hub. Master UH-60 Black Hawk NVG operations—including combat SAR, external load delivery, and covert insertion—across 320+ annual training days. Our 38-acre complex, adjacent to Kennedy Space Center, offers unrestricted airspace for high-tempo missions within 10NM of base, with NATO-recognized certifications
     `,
-    buttons: [
-        {
-        name: "Enroll Now",
-        link: "/#",
-        primary: true,
-        },
-        {
-        name: "Contact Us",
-        link: "/#",
-        primary: false,
-        }
-    ],
+  buttons: [
+    {
+      name: "Enroll Now",
+      link: "/#",
+      primary: true,
+    },
+  ],
 };
 
-export default CTAMilitary;
+export default militaryCta;
