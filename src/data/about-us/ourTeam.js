4599e757d2cb952a48799f8f222ba07fb1486cb3
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-16 14:42:05 -0300

    Update ourTeam data

--- src/data/about-us/ourTeam.js
+++ src/data/about-us/ourTeam.js
@@ -4,16 +4,33 @@ const ourTeam = {
     upperHeading: "Meet Our Team",
     heading: "Our Leadership Team",
     description:
-      "",
+      "The leadership team at USATS brings decades of aviation expertise from military, commercial, and FAA training backgrounds. With 30+ years of shaping pilots from 120+ countries—and a fleet spanning Cessna 172s to UH-60 Black Hawks—we’re committed to delivering world-class flight education with precision, safety, and a focus on your success",
     members: [
       {
-        name: "",
+        name: "Brian Craddock",
         imagePath:
           "/src/assets/placeholder-person.jpg",
         imageAlt:
           "",
-        bio: "",
-        rank: "",
+        bio: "Brian Craddock is an experienced aviation executive with extensive leadership experience in flight training, aviation program management, and government contracting. As Managing Director and CEO of US Aviation Training Solutions, he oversees flight training operations, aircraft maintenance, and FAA compliance, ensuring the success of both domestic and international aviation training programs. His expertise spans strategic planning, workforce optimization, and regulatory adherence, with a focus on delivering high-quality aviation solutions in challenging environments. Prior to his current role, Brian held key leadership positions within the U.S. Department of State and the U.S. Army, where he managed multi-billion-dollar aviation programs, oversaw aircraft operations across multiple countries, and led acquisition strategies for advanced aviation platforms.",
+        rank: "Managing Director",
+      },
+      {
+        name: "Rey Deptula",
+        imagePath:
+          "/src/assets/placeholder-person.jpg",
+        imageAlt:
+          "",
+        bio: "Ray is a retired career naval officer and naval aviator with 3,200 flight hours and over 3,000 shipboard landings, flying mainly SH-60B Seahawk and UH-1N Huey helicopters. He was a flight instructor for SH-60Bs and managed the navy’s east coast shipboard landing instruction program. Ray also holds an FAA Airline Transport Pilot (ATP) Rotorcraft-Helicopter certification.",
+        rank: "Business Development Manager",
+      },
+      {
+        name: "Stephane Rebeix",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+          "US Aviation Training Solutions Certified Flight Instructor Stephane Rebeix",
+        bio: "Stephane Rebeix, USATS Chief Flight Instructor, is a master helicopter pilot with 9,000+ flight hours (4,200+ instructing) across offshore, military, and training operations. An FAA 141 VFR/IFR expert since 2018, he’s flown AS332L1/2, EC225, and S76C+/++ helicopters globally—from Nigeria’s oil fields to the North Sea. Holder of dual ATP (US/Europe), CFI/CFII, and business degrees, he merges technical precision with academic rigor to shape elite aviators.",
+        rank: "Chief Flight Instructor",
       },
     ],
   },
