11b9d06f8bdb2222de228a4394bffe848bd25a4a
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-16 12:46:52 -0300

    Add ourTeam data

---
+++ src/data/about-us/ourTeam.js
@@ -0,0 +1,59 @@
+
+const ourTeam = { 
+    staffGrid: {
+    upperHeading: "Meet Our Team",
+    heading: "Our Leadership Team",
+    description:
+      "",
+    members: [
+      {
+        name: "",
+        imagePath:
+          "/src/assets/placeholder-person.jpg",
+        imageAlt:
+          "",
+        bio: "",
+        rank: "",
+      },
+    ],
+  },
+
+  teamGrid: {
+    upperHeading: "Meet Our Team",
+    heading: "The Airframe of US Aviation Training Solutions",
+    description:
+      "USATS instructors are FAA-certified professionals with decades of real-world experience, including military and commercial aviation. Trained in advanced safety systems like CRM and TEM, our team provides personalized flight instruction using modern aircraft and a proven curriculum refined over 30 years.",
+    members: [
+      {
+        name: "Brian Craddock",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+        "US Aviation Training Solutions Certified Flight Instructor Brian Craddock",
+        rank: "Flight Instructor Managing Director",
+      },
+      {
+        name: "Jason Krum",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+          "US Aviation Training Solutions Certified Flight Instructor Jason Krum",
+        rank: "Flight Instructor",
+      },
+      {
+        name: "Rey Deptula",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+          "US Aviation Training Solutions Certified Flight Instructor Rey Deptula",
+        rank: "Flight Instructor Business Development Manager",
+      },
+      {
+        name: "Stephane Rebeix",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+          "US Aviation Training Solutions Certified Flight Instructor Stephane Rebeix",
+        rank: "Chief Flight Instructor",
+      },
+    ],
+  },
+}
+
+export default ourTeam
