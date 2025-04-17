017288977da11939aef67da8adae9e3ebeb9574c
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-17 16:21:02 -0300

    Update our team page

--- src/data/about-us/ourTeam.js
+++ src/data/about-us/ourTeam.js
@@ -42,32 +42,46 @@ const ourTeam = {
       "USATS instructors are FAA-certified professionals with decades of real-world experience, including military and commercial aviation. Trained in advanced safety systems like CRM and TEM, our team provides personalized flight instruction using modern aircraft and a proven curriculum refined over 30 years.",
     members: [
       {
-        name: "Brian Craddock",
+        name: "Joe Doe",
         imagePath: "/src/assets/placeholder-person.jpg",
         imageAlt:
-        "US Aviation Training Solutions Certified Flight Instructor Brian Craddock",
-        rank: "Flight Instructor Managing Director",
+        "",
+        rank: "Airplane Instructor",
       },
       {
-        name: "Jason Krum",
+        name: "Joe Doe",
         imagePath: "/src/assets/placeholder-person.jpg",
         imageAlt:
-          "US Aviation Training Solutions Certified Flight Instructor Jason Krum",
-        rank: "Flight Instructor",
+        "",
+        rank: "Airplane Instructor",
       },
       {
-        name: "Rey Deptula",
+        name: "Joe Doe",
         imagePath: "/src/assets/placeholder-person.jpg",
         imageAlt:
-          "US Aviation Training Solutions Certified Flight Instructor Rey Deptula",
-        rank: "Flight Instructor Business Development Manager",
+        "",
+        rank: "Airplane Instructor",
       },
       {
-        name: "Stephane Rebeix",
+        name: "Joe Doe",
         imagePath: "/src/assets/placeholder-person.jpg",
         imageAlt:
-          "US Aviation Training Solutions Certified Flight Instructor Stephane Rebeix",
-        rank: "Chief Flight Instructor",
+        "",
+        rank: "Helicopter Instructor",
+      },
+      {
+        name: "Joe Doe",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+        "",
+        rank: "Helicopter Instructor",
+      },
+      {
+        name: "Joe Doe",
+        imagePath: "/src/assets/placeholder-person.jpg",
+        imageAlt:
+        "",
+        rank: "Helicopter Instructor",
       },
     ],
   },
