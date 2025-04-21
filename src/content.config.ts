7ed72cf47313bff5f2e4bdfc16440d8e689dc898
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-21 12:11:34 -0300

    Fix blog post slug

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,27 +1,7 @@
-// import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-// const blogCollection = defineCollection({
-//   type: "content",
-//   // Type-check frontmatter using a schema
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     author: z.string(),
-//     readingTime: z.number(),
-//     // Transform string to Date object
-//     pubDate: z.coerce.date(),
-//     updatedDate: z.coerce.date().optional(),
-//     heroImage: z.string().optional(),
-//     category: z.string(),
-//     tags: z.array(z.string()).optional(),
-//   }),
-// });
 
 const blogCollection = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
