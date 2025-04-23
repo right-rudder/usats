d22827f63962cda4469b132e0575c0893a10e018
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-23 00:36:33 -0300

    Update ourfleet

--- src/data/about-us/fleet.js
+++ src/data/about-us/fleet.js
@@ -1,4 +1,4 @@
-export const fleet = [
+export const airplaneFleet = [
     {
       name: "Cessna 172",
       slug: "cessna172",
@@ -51,80 +51,83 @@ export const fleet = [
         ""
       ],
     },
-    {
-      name: "UH-60 Black Hawk",
-      slug: "uh60blackhawk",
-      rentalPrice: "1",
-      usableFuel: "",
-      model: "UH-60",
-      equipment: [
-        "Transport",
-        "Search & Rescue",
-        "External Load",
-        "Medevac",
-        "Fire Fighting",
-        "Executive Transport Missions",
-      ],
-      imagePath: "/src/assets/UH-60_Black_Hawk_1.webp",
-      imagesArray: [
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-      ],
-      description: [
-        "The Best Multi-Mission Helicopter",
-        ""
-      ],
-    },
-    {
-      name: "Robinson R44 Fleet",
-      slug: "robinsonr44fleet",
-      rentalPrice: "1",
-      usableFuel: "",
-      model: "UH-60",
-      equipment: [
-        "Semi-Rigid",
-        "IFR equipped",
-        "G500H EFIS",
-        "GPS WAAS 430",
-      ],
-      imagePath: "/src/assets/Robinson_R44_USATS_Ramp_ee7c0db4-345b-4afc-ad3d-dadee593e801.jpg",
-      imagesArray: [
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-      ],
-      description: [
-        "The Best Multi-Mission Helicopter",
-        ""
-      ],
-    },
-    {
-      name: "Schweizer S-300",
-      slug: "schweizers300",
-      rentalPrice: "1",
-      usableFuel: "",
-      model: "UH-60",
-      equipment: [
-        "Fully Articulated",
-        "IFR Equipped",
-        "GI 275 Attitude Indicator & HSI",
-        "GPS WAAS G430 & GTN 650",
-      ],
-      imagePath: "/src/assets/Robinson_R44_USATS_Ramp_ee7c0db4-345b-4afc-ad3d-dadee593e801.jpg",
-      imagesArray: [
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-        "/src/assets/placeholder.webp",
-      ],
-      description: [
-        "The Best Multi-Mission Helicopter",
-        ""
-      ],
-    },
   ];
+
+export const helicopterFleet = [
+  {
+    name: "UH-60 Black Hawk",
+    slug: "uh60blackhawk",
+    rentalPrice: "1",
+    usableFuel: "",
+    model: "UH-60",
+    equipment: [
+      "Transport",
+      "Search & Rescue",
+      "External Load",
+      "Medevac",
+      "Fire Fighting",
+      "Executive Transport Missions",
+    ],
+    imagePath: "/src/assets/UH-60_Black_Hawk_1.webp",
+    imagesArray: [
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+    ],
+    description: [
+      "The Best Multi-Mission Helicopter",
+      ""
+    ],
+  },
+  {
+    name: "Robinson R44 Fleet",
+    slug: "robinsonr44fleet",
+    rentalPrice: "1",
+    usableFuel: "",
+    model: "R44",
+    equipment: [
+      "Semi-Rigid",
+      "IFR equipped",
+      "G500H EFIS",
+      "GPS WAAS 430",
+    ],
+    imagePath: "/src/assets/Robinson_R44_USATS_Ramp_ee7c0db4-345b-4afc-ad3d-dadee593e801.jpg",
+    imagesArray: [
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+    ],
+    description: [
+      "The Best Multi-Mission Helicopter",
+      ""
+    ],
+  },
+  {
+    name: "Schweizer S-300",
+    slug: "schweizers300",
+    rentalPrice: "2",
+    usableFuel: "",
+    model: "S-300",
+    equipment: [
+      "Fully Articulated",
+      "IFR Equipped",
+      "GI 275 Attitude Indicator & HSI",
+      "GPS WAAS G430 & GTN 650",
+    ],
+    imagePath: "/src/assets/Robinson_R44_USATS_Ramp_ee7c0db4-345b-4afc-ad3d-dadee593e801.jpg",
+    imagesArray: [
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+      "/src/assets/placeholder.webp",
+    ],
+    description: [
+      "The Best Multi-Mission Helicopter",
+      ""
+    ],
+  },
+];
 
   // 2 alto NG: dynon hdx1200 dual displays, garmin 175s (lower operating cost)
