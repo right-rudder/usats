882c420f666ae45c28323215ffd7ca99df939cb7
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-04-25 18:43:42 -0300

    Add careers data

---
+++ src/data/services/career.js
@@ -0,0 +1,48 @@
+import { COMPANY_NAME, COMPANY_NICKNAME } from "../../consts";
+
+const careers = {
+
+    title: "Work with us!",
+    subtitle: "Careers",
+
+    paragraphs: "",
+
+
+    openPositions: {
+        title: "Open Positions",
+        Maintenance: {
+            title: "Maintenance",
+            openPositions: [
+                {
+                    title: "Maintenance Technician",
+                    link: "",
+                }
+            ],
+        },
+        buttons: [
+            {
+              name: "Directions",
+              link: "https://www.google.com/maps/place/US+Aviation+Jet+Center/@28.5108366,-80.7935879,17z/data=!3m1!4b1!4m6!3m5!1s0x88e0ad45bfc65527:0x83a84771beabce22!8m2!3d28.5108366!4d-80.7935879!16s%2Fg%2F11fp5g3c02?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+              primary: true,
+            },
+          ],
+    },
+
+    howToApply: {
+        title: "How to Apply",
+        paragraphs: [
+            "Send your resume to the email listed below and please make sure to include the job position you are applying for.",
+            "For employment verification please email."
+        ],
+        
+        buttons: [
+            {
+              name: "HR@usatsolutions.com",
+              link: "https://www.google.com/maps/place/US+Aviation+Jet+Center/@28.5108366,-80.7935879,17z/data=!3m1!4b1!4m6!3m5!1s0x88e0ad45bfc65527:0x83a84771beabce22!8m2!3d28.5108366!4d-80.7935879!16s%2Fg%2F11fp5g3c02?entry=ttu&g_ep=EgoyMDI1MDQyMy4wIKXMDSoASAFQAw%3D%3D",
+              primary: true,
+            },
+        ],
+    }
+};
+
+export default careers;
