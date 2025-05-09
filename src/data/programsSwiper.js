9254ddc5743e9465b7f5cea62fed3a5a823cc638
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-05-09 16:21:09 -0300

    Update Flight training page

--- src/data/programsSwiper.js
+++ src/data/programsSwiper.js
@@ -7,19 +7,19 @@ const programsSwiper = {
       "Your aviation dreams take center stage here. Whether pursuing a professional career, building flight hours, or seeking leisurely adventure, we have the perfect path for you.",
     cards: [
       {
-        title: "Airplane Pilot Training Programs",
+        title: "Airplane Training Programs",
         imagePath: imgAirplaneCard.imagePath,
         imageAlt: imgAirplaneCard.imageAlt,
         description:
-          "This program is designed for students with zero flying experience. You will obtain your FAA Private, Instrument, Commercial license, CFI, and CFII licenses.",
+          "From zero experience to certified instructor, our comprehensive courses include FAA Private, Instrument, Commercial, CFI, and CFII licenses, with customized options for experienced students.",
         link: "/airplane-training",
       },
       {
-        title: "Helicopter Pilot Training Programs",
+        title: "Helicopter Training Programs",
         imagePath: imgHelicopterCard.imagePath,
         imageAlt: imgHelicopterCard.imageAlt,
         description:
-          "With an average of 320 flying days per year, we can make sure your training will be done on time. Our practice areas are located within 10 nautical miles from our airport, limiting the transit time for your off-airport training.",
+          "Start from zero or build on your experience as you earn your FAA Private, Instrument, Commercial, CFI, CFII, and ATP-L helicopter licenses, with flexible training paths available.",
         link: "/helicopter-training",
       },
     ],
