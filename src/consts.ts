324badca72b824c17b2a7653f752dd63ef335cb4
Raul Ospina <raminka13@hotmail.com>
2025-05-12 19:20:23 -0500

    Update: OG image

--- src/consts.ts
+++ src/consts.ts
@@ -3,15 +3,17 @@ export const SITE_TITLE =
   "Airplane and Helicopter Flight Training in Titusville, FL | USATS";
 
 // CHANGE default meta description -> should only be used in homepage technically, but is there for a fallback in case a page is missed
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION =
+  "Launch your aviation career with USATS in Titusville, FL. Offering FAA-certified helicopter and airplane training, modern fleet, and international student support. Trusted by pilots from 120+ countries.";
 
 //SEO tip -> keyword + city 1
 //Should have between 5-10 different localities to mix and match keywords
 //typical keywords = how to become a pilot, flight school near, private pilot, discovery flight, introductory flight, instrument rating, commercial certificate, certified flight instructor, airline programs, how to become an airline pilot
 // CHANGE default keywords -> should only be used in homepage technically, but is there for a fallback in case a page is missed
-export const KEYWORDS = "";
+export const KEYWORDS =
+  "flight school, pilot training, aviation school, helicopter pilot training, aviation academy, aviation courses, aviation training, aviation school near me, pilot school near me, aviation training institute, private pilot license training Florida, commercial pilot course cost, FAA-certified flight school in Florida, accelerated flight training program, helicopter flight school Florida, flight school for international students, best flight school for veterans, airline transport pilot license training, Part 141 flight school Florida, military helicopter pilot training USA, flight school Titusville FL, pilot training near Cocoa Beach, helicopter training Merritt Island, aviation school near Viera Florida, flight training Space Coast FL, airplane flight school Orlando area, helicopter pilot training Central Florida, discovery flight Florida, learn to fly a plane, how to become a pilot, pilot lessons near me, introductory flight lesson Florida, flight school for beginners, FAA Part 141 flight school, Part 61 vs Part 141 flight training, flight instructor certification Florida, instrument rating course Florida, aircraft maintenance technician training, flight school for international students USA, M1 visa flight training program, aviation training for international students, pilot training with visa support USA, aviation academy, helicopter pilot training, business jet, ultralight aircraft for sale, aviation courses after 12th, pilot school, private pilot licence, pilot training, pilot courses, aviation school, pilot institute, pilot shop, private pilot training, pilot academy, best flight schools, best pilot schools, online flight school, pilot shop near me, aviation training, learn to fly a plane, flight training, pilot lessons near me, airline pilot training, aviation technician, aviation maintenance technician, aviation degree, aircraft technician, aviation management courses, private pilot, flight instructor, aviation management, aviation maintenance, aviation engineering, aviation headset, aviation mechanic, commercial pilot training, part 141 flight schools, aviation training institute, aviation academy, aviation school near me, pilot training near me, aviation courses, flight school near me, flying lessons near me, flight lessons near me, flight training near me, private pilot licence, pilot school near me, aviation mechanic school, airplane mechanic school, aviation management, aircraft maintenance, airline pilot, flight safety, commercial pilot licence, pilot training cost, cabin crew courses, aircraft maintenance technician, aviation headset, discovery flight, drone pilot training, aviation engineering, pilots licence cost, become a pilot, discovery flight near me, used aircraft for sale, airport management courses, flight museum, aviation mechanic, commercial pilot school, pilot headsets, avionics technician, aviation shop, aircraft charter, aviation consulting, aerospace companies near me, aviation courses after 12th, aviation maintenance technician, aviation degree, aircraft technician, aviation training institute, aviation management degree, commercial pilot training, learn to fly a plane, aviation training, light aircraft for sale, flying academy, pilot lessons near me, aviation technician, best aviation headset, airline pilot training";
 
-export const OG_IMAGE = "/USATS-OG-image.jpg";
+export const OG_IMAGE = "/USATS-OG-image.webp";
 
 export const GEO_LATITUDE = 28.51012890041488;
 export const GEO_LONGITUDE = -80.79335517556147;
@@ -35,11 +37,12 @@ export const ADDRESS_LINE_1 = "355 Golden Knights Blvd";
 export const ADDRESS_LINE_2 = "Titusville, FL 32780";
 export const FACEBOOK_URL = "https://www.facebook.com/USATSolutions/";
 export const INSTAGRAM_URL = "https://www.instagram.com/usatsolutions/";
-export const LINKEDIN_URL = "https://www.linkedin.com/company/us-aviation-training-solutions";
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/company/us-aviation-training-solutions";
 export const YOUTUBE_URL = null;
 export const TWITTER_URL = null;
 export const EMAIL_ADDRESS = "info@usatsolutions.com";
-export const COMPANY_SITE_LINK="https://usatsflighttraining.com";
+export const COMPANY_SITE_LINK = "https://usatsflighttraining.com";
 
 export const COMPANY_NICKNAME = "USATS";
 export const ADDRESS = "355 Golden Knight Blvd";
@@ -48,34 +51,48 @@ export const ADDRESS_CITY = "Titusville";
 export const ADDRESS_ZIP = "32780";
 export const AIRPORT = "SPACE COAST REGIONAL";
 export const AIRPORT_CODE = "KTIX";
-export const AIRPORT_GOOGLE_LOCATION = "https://www.google.com/maps/place/Space+Coast+Regional+Airport/@28.5123353,-80.7997203,16.22z/data=!4m6!3m5!1s0x88e0ad2310d73cfb:0x4bcd770a5e53125e!8m2!3d28.5133625!4d-80.7988013!16zL20vMDNmZnAy?entry=ttu&g_ep=EgoyMDI1MDUwNy4wIKXMDSoASAFQAw%3D%3D"
+export const AIRPORT_GOOGLE_LOCATION =
+  "https://www.google.com/maps/place/Space+Coast+Regional+Airport/@28.5123353,-80.7997203,16.22z/data=!4m6!3m5!1s0x88e0ad2310d73cfb:0x4bcd770a5e53125e!8m2!3d28.5133625!4d-80.7988013!16zL20vMDNmZnAy?entry=ttu&g_ep=EgoyMDI1MDUwNy4wIKXMDSoASAFQAw%3D%3D";
 
 // Pages Data
 
 export const BANNER_TITLE = `Experience flight like never before.`;
 
+// Financing
 
-// Financing 
-
-export const STUDENT_LOAN_APPLICATION = "https://apply.stratus.finance/usaviationtraining818394";
-export const STRATUS_FILES = 'https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL';
-export const STRATUS_ADVANTAGE = "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=d995ca1a-b9cb-4f2f-acfb-e31817dce5df";
-export const STRATUS_PAGE_FLYER = "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=13f2bdd3-cab1-45db-9965-003e4e34de03";
-export const STRATUS_QR_CODE = "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=454f0b87-bd29-452f-86b4-0638e0058349";
-export const STRATUS_TRIFOLD = "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=0a87f56f-8e7c-40a2-8d17-8826317aba39";
-
+export const STUDENT_LOAN_APPLICATION =
+  "https://apply.stratus.finance/usaviationtraining818394";
+export const STRATUS_FILES =
+  "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL";
+export const STRATUS_ADVANTAGE =
+  "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=d995ca1a-b9cb-4f2f-acfb-e31817dce5df";
+export const STRATUS_PAGE_FLYER =
+  "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=13f2bdd3-cab1-45db-9965-003e4e34de03";
+export const STRATUS_QR_CODE =
+  "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=454f0b87-bd29-452f-86b4-0638e0058349";
+export const STRATUS_TRIFOLD =
+  "https://stratusfinancial.egnyte.com/fl/tY9DWLsulT#folder-link/DIGITAL?p=0a87f56f-8e7c-40a2-8d17-8826317aba39";
 
 // Application
-export const STUDENT_APPLICATION_CHECKLIST_PDF = "https://cdn.shopify.com/s/files/1/0783/7207/7845/files/US_STUDENT_REGISTRATION_CHECKLIST_40b3383e-b01b-47f1-8065-3f6ed1e8eb67.pdf?v=1740581271"
-export const STUDENT_APPLICATION_PDF = "https://cdn.shopify.com/s/files/1/0783/7207/7845/files/US_APPLICATION.pdf?v=1740583077";
-export const INTERNATIONAL_STUDENTS_CHECKLIST = "https://cdn.shopify.com/s/files/1/0783/7207/7845/files/INTERNATIONAL_REGISTRATION.pdf?v=1740583096"
-export const HOW_STUDY_IN_US = "https://studyinthestates.dhs.gov"
+export const STUDENT_APPLICATION_CHECKLIST_PDF =
+  "https://cdn.shopify.com/s/files/1/0783/7207/7845/files/US_STUDENT_REGISTRATION_CHECKLIST_40b3383e-b01b-47f1-8065-3f6ed1e8eb67.pdf?v=1740581271";
+export const STUDENT_APPLICATION_PDF =
+  "https://cdn.shopify.com/s/files/1/0783/7207/7845/files/US_APPLICATION.pdf?v=1740583077";
+export const INTERNATIONAL_STUDENTS_CHECKLIST =
+  "https://cdn.shopify.com/s/files/1/0783/7207/7845/files/INTERNATIONAL_REGISTRATION.pdf?v=1740583096";
+export const HOW_STUDY_IN_US = "https://studyinthestates.dhs.gov";
 
 // Video links
 
-export const VIDEOLINK_HOME = "https://cdn.shopify.com/videos/c/o/v/f7fe5a4231a94c38a91db2db8280be4e.mp4";
-export const VIDEOLINK_AIRTOUR = "https://cdn.shopify.com/videos/c/o/v/2e1eb45cc3e041bbb8f12d2b94d11067.mp4";
-export const VIDEOLINK_AIRPLANES = "https://cdn.shopify.com/videos/c/o/v/6b057798c5be41b9865d7a4054736b45.mp4";
-export const VIDEO_HOME_HELICOPTER = "https://cdn.shopify.com/videos/c/o/v/41e966eb47fa4d8eaad7f2b713b2459d.mp4";
-export const VIDEOLINK_MAINTENANCE = "https://cdn.shopify.com/videos/c/o/v/40ea4b0895e0415fa098733bcf6e8ee4.mp4";
-export const VIDEOLINK_ABOUTUS = "https://usatsflighttraining.com/cdn/shop/videos/c/vp/9cce912410e44240829a51b85a4ce623/9cce912410e44240829a51b85a4ce623.HD-1080p-2.5Mbps-42396578.mp4?v=0";
+export const VIDEOLINK_HOME =
+  "https://cdn.shopify.com/videos/c/o/v/f7fe5a4231a94c38a91db2db8280be4e.mp4";
+export const VIDEOLINK_AIRTOUR =
+  "https://cdn.shopify.com/videos/c/o/v/2e1eb45cc3e041bbb8f12d2b94d11067.mp4";
+export const VIDEOLINK_AIRPLANES =
+  "https://cdn.shopify.com/videos/c/o/v/6b057798c5be41b9865d7a4054736b45.mp4";
+export const VIDEO_HOME_HELICOPTER =
+  "https://cdn.shopify.com/videos/c/o/v/41e966eb47fa4d8eaad7f2b713b2459d.mp4";
+export const VIDEOLINK_MAINTENANCE =
+  "https://cdn.shopify.com/videos/c/o/v/40ea4b0895e0415fa098733bcf6e8ee4.mp4";
+export const VIDEOLINK_ABOUTUS =
+  "https://usatsflighttraining.com/cdn/shop/videos/c/vp/9cce912410e44240829a51b85a4ce623/9cce912410e44240829a51b85a4ce623.HD-1080p-2.5Mbps-42396578.mp4?v=0";
