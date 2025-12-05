22e88b56abac654849f1858b27fb9ff5ca64cf21
lvcaspacifico <lucasreinaldopacificoh@gmail.com>
2025-12-05 19:35:05 -0300

    Adds links for the Twitter/X profile and Youtube channel

--- src/consts.ts
+++ src/consts.ts
@@ -40,8 +40,8 @@ export const FACEBOOK_URL = "https://www.facebook.com/USATSolutions/";
 export const INSTAGRAM_URL = "https://www.instagram.com/usatsolutions/";
 export const LINKEDIN_URL =
   "https://www.linkedin.com/company/us-aviation-training-solutions";
-export const YOUTUBE_URL = null;
-export const TWITTER_URL = null;
+export const YOUTUBE_URL = "https://www.youtube.com/@u.s.aviationtrainingsoluti7225";
+export const TWITTER_URL = "https://x.com/USATSolutions";
 export const EMAIL_ADDRESS = "info@usatsolutions.com";
 export const COMPANY_SITE_LINK = "https://usatsflighttraining.com";
 
